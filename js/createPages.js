d4656ff355920054857005304722e5dd75f11604
pierdr <me@pierdr.com>
2022-04-10 00:43:11 -0700

    add subfolder again

--- js/createPages.js
+++ js/createPages.js
@@ -13,24 +13,24 @@ $(function(){
   // });
 
   //REFERENCE
-  $("#preview-5 p.overlay__content").html("<iframe src=\"Reference.html\" title=\"Getting Started - Processing\"></iframe>");
+  $("#preview-5 p.overlay__content").html("<iframe src=\"subPages/Reference.html\" title=\"Getting Started - Processing\"></iframe>");
   //PROCESSING
-  $("#preview-4 p.overlay__content").html("<iframe src=\"Getting_started_Processing.html\" title=\"Getting Started - Processing\"></iframe>");
+  $("#preview-4 p.overlay__content").html("<iframe src=\"subPages/Getting_started_Processing.html\" title=\"Getting Started - Processing\"></iframe>");
   //JAVASCRIPT
-  $("#preview-3 p.overlay__content").html("<iframe src=\"GettingStartedJavascript.html\" title=\"Getting Started - Javascript\"></iframe>");
+  $("#preview-3 p.overlay__content").html("<iframe src=\"subPages/GettingStartedJavascript.html\" title=\"Getting Started - Javascript\"></iframe>");
   //P5JS
-  $("#preview-1 p.overlay__content").html("<iframe src=\"GettingStarted_P5JS.html\" title=\"Getting Started - P5JS\"></iframe>");
+  $("#preview-1 p.overlay__content").html("<iframe src=\"subPages/GettingStarted_P5JS.html\" title=\"Getting Started - P5JS\"></iframe>");
   //TramontanaCV
-  $("#preview-2 p.overlay__content").html("<iframe src=\"TramontanaCV.html\" title=\"TramontanaCV\"></iframe>");
+  $("#preview-2 p.overlay__content").html("<iframe src=\"subPages/TramontanaCV.html\" title=\"TramontanaCV\"></iframe>");
   //Gallery
-  $("#preview-6 p.overlay__content").html("<iframe src=\"Gallery.html\" title=\"Gallery\"></iframe>");
+  $("#preview-6 p.overlay__content").html("<iframe src=\"subPages/Gallery.html\" title=\"Gallery\"></iframe>");
   // //Tutorials
-  $("#preview-9 p.overlay__content").html("<iframe src=\"Tutorials.html\" title=\"Tutorials\"></iframe>");
+  $("#preview-9 p.overlay__content").html("<iframe src=\"subPages/Tutorials.html\" title=\"Tutorials\"></iframe>");
   // Downloads
-   $("#preview-7 p.overlay__content").html("<iframe src=\"Downloads.html\" title=\"Downloads\"></iframe>");
+   $("#preview-7 p.overlay__content").html("<iframe src=\"subPages/Downloads.html\" title=\"Downloads\"></iframe>");
    // About
-  $("#preview-12 p.overlay__content").html("<iframe src=\"About.html\" title=\"About us\"></iframe>");
+  $("#preview-12 p.overlay__content").html("<iframe src=\"subPages/About.html\" title=\"About us\"></iframe>");
   // OpenFrameworks
- $("#preview-11 p.overlay__content").html("<iframe src=\"OpenFrameworks.html\" title=\"ofxTramontana\"></iframe>");
+ $("#preview-11 p.overlay__content").html("<iframe src=\"subPages/OpenFrameworks.html\" title=\"ofxTramontana\"></iframe>");
 
 });
