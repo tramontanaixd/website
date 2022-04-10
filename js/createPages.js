1d0f23c62d66309d06537cf06ffc35e80ce0cb2e
pierdr <me@pierdr.com>
2022-04-10 00:27:35 -0700

    added web files

---
+++ js/createPages.js
@@ -0,0 +1,36 @@
+$(function(){
+
+
+  // var url = '../subpages/gettingstarted.json';
+  // var json = $.getJSON(url, function(result){
+  //   console.log(result);
+  //   $("#preview-5 p.overlay__content").html(result.content);
+  // });
+  // var url = '../mdPages/gettingstarted.json';
+  // var json = $.getJSON(url, function(result){
+  //   console.log(result);
+  //   $("#preview-5 p.overlay__content").html(result.content);
+  // });
+
+  //REFERENCE
+  $("#preview-5 p.overlay__content").html("<iframe src=\"subPages/Reference.html\" title=\"Getting Started - Processing\"></iframe>");
+  //PROCESSING
+  $("#preview-4 p.overlay__content").html("<iframe src=\"subPages/Getting_started_Processing.html\" title=\"Getting Started - Processing\"></iframe>");
+  //JAVASCRIPT
+  $("#preview-3 p.overlay__content").html("<iframe src=\"subpages/GettingStartedJavascript.html\" title=\"Getting Started - Javascript\"></iframe>");
+  //P5JS
+  $("#preview-1 p.overlay__content").html("<iframe src=\"subpages/GettingStarted_P5JS.html\" title=\"Getting Started - P5JS\"></iframe>");
+  //TramontanaCV
+  $("#preview-2 p.overlay__content").html("<iframe src=\"subPages/TramontanaCV.html\" title=\"TramontanaCV\"></iframe>");
+  //Gallery
+  $("#preview-6 p.overlay__content").html("<iframe src=\"subPages/Gallery.html\" title=\"Gallery\"></iframe>");
+  // //Tutorials
+  $("#preview-9 p.overlay__content").html("<iframe src=\"subPages/Tutorials.html\" title=\"Tutorials\"></iframe>");
+  // Downloads
+   $("#preview-7 p.overlay__content").html("<iframe src=\"subPages/Downloads.html\" title=\"Downloads\"></iframe>");
+   // About
+  $("#preview-12 p.overlay__content").html("<iframe src=\"subPages/About.html\" title=\"About us\"></iframe>");
+  // OpenFrameworks
+ $("#preview-11 p.overlay__content").html("<iframe src=\"subPages/OpenFrameworks.html\" title=\"ofxTramontana\"></iframe>");
+
+});
